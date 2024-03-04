59f6bbf41607eacb4ef26840e9be919c785a0a86
RW <rwu@DESKTOP-EAOTKCT>
2024-03-04 15:31:43 -0500

    done

---
+++ main.js
@@ -0,0 +1,18 @@
+/*create a message first, then display the combo in a box window or webpage
+*/
+
+let msg='You have received this message because you have been chosen to open an important vault';
+alert(`${msg}`);
+
+// var num1,num2,num3;/var also works
+let num1=20/2;
+let num2=5*8;
+let num3=40-1;
+console.log(num1,num2,num3);
+
+window.alert("Combo1: " + num1 + ", Combo2: " + num2 + ", Combe3: " + num3);
+// testing to transfer the combo to the HTML contect instead of a alert window
+// const outputElement = document.getElementById('output');
+// outputElement.textContent = "Combo1: " + num1 + ", Combo2: " + num2 + ", Combo3: " + num3;
+
+
